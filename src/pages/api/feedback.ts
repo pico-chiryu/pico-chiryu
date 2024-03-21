9f6d492c96ebc0148a0e36d63bd15ea50af69eee
ひらとしんじ <snj.hirato@gmail.com>
2024-03-21 16:48:29 +0900

    mailer実装

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,11 +1,12 @@
 import type { APIRoute } from "astro";
+import nodemailer from 'nodemailer';
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const name = data.get("name");
   const email = data.get("email");
   const message = data.get("message");
-  // Validate the data - you'll probably want to do more than this
+
   if (!name || !email || !message) {
     return new Response(
       JSON.stringify({
@@ -14,11 +15,36 @@ export const POST: APIRoute = async ({ request }) => {
       { status: 400 }
     );
   }
-  // Do something with the data, then return a success response
-  return new Response(
-    JSON.stringify({
-      message: "Success!"
-    }),
-    { status: 200 }
-  );
+
+  // nodemailer を使用してメール送信の設定を行う
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // 使っているメールサービス
+    auth: {
+      user: 'snj.hirato@gmail.com', // あなたのメールアドレス
+      pass: 'qtrzhpsylkearbsg', // あなたのメールパスワード
+    },
+  });
+
+  const mailOptions = {
+    from: 'snj.hirato@gmail.com',
+    to: 'snj.hirato@gmail.com', // 受信者のメールアドレス
+    subject: 'New Contact Message',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+  };
+
+  try {
+    // メール送信
+    await transporter.sendMail(mailOptions);
+    return new Response(
+      JSON.stringify({ message: "Success!" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    return new Response(
+      JSON.stringify({ message: "Failed to send email" }),
+      { status: 500 }
+    );
+  }
 };
