eb8faad2b26594c544ad604999c723c3f2dc7ea7
ひらとしんじ <snj.hirato@gmail.com>
2024-05-16 14:26:06 +0900

    あいさつを上部へ

--- src/components/Thanks.tsx
+++ src/components/Thanks.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Thanks() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "80vh", backgroundColor: "#BEFEE0", color: "#215364", textAlign: "center" }}>
-      <h1 className="text-4xl font-bold mb-8">ありがとうございました！</h1>
+      <h1 className="text-2xl font-bold mb-8">ありがとうございました！</h1>
       <p className="text-xl mb-12">
         お問い合わせいただき、誠にありがとうございます。
         <br />
